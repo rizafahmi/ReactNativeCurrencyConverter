7cffebd9a41733a7f9fd87efb1dd0fce5071eb53
Riza Fahmi <rizafahmi@gmail.com>
2017-06-18 09:17:32 +0700

    Finish HomeScreen Container

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,2 +1,11 @@
+import React from 'react'
+import { StatusBar, Text } from 'react-native'
 
+import { Container } from '../components/Container'
 
+export default () => (
+  <Container>
+    <StatusBar translucent barStyle='light-content' />
+    <Text>Home.js</Text>
+  </Container>
+)

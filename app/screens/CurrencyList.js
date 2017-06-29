1dfb08812527d74fee77549b8f6b6d42c53efd50
Riza Fahmi <rizafahmi@gmail.com>
2017-06-29 16:05:00 +0700

    Add CurrencyList screen

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,2 +1,15 @@
+import React from 'react'
+import { View, FlatList, Text, StatusBar } from 'react-native'
 
+import currencies from '../data/currencies'
 
+export default props => (
+  <View style={{ flex: 1 }}>
+    <StatusBar barStyle='default' translucent />
+    <FlatList
+      data={currencies}
+      keyExtractor={item => item}
+      renderItem={({ item }) => <Text>{item}</Text>}
+    />
+  </View>
+)
